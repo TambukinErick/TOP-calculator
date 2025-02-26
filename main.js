100b0208659a9dade9603629819ba6d5acd8ff09
erick <124009200+TambukinErick@users.noreply.github.com>
2025-02-26 11:04:07 +0800

    Create basic mathematical operation functions: add, subtract, multiply, divide

---
+++ main.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(add(1, 1));
+console.log(minus(1, 1));
+console.log(divide(2, 1));
+console.log(multiply(1, 2));
