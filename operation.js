ab07e96a81b72124f2cc8ae46503b8eea0b33ffd
erick <124009200+TambukinErick@users.noreply.github.com>
2025-02-26 17:21:34 +0800

    Separate computation logic from main js file

---
+++ operation.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+
+function operate(a, operator, b) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return minus(a, b);
+  } else if (operator === "*") {
+    return multiply(a, b);
+  } else if (operator === "/") {
+    return (b !== 0) ? divide(a, b) : "Dividing by 0 is not allowed";
+  }
+}
+
+export {add, minus, divide, multiply, operate};
